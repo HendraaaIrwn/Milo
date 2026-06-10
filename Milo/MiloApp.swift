3f8ce0f69c8944dff41f353eb83ca846445b7411
HendraaaIrwn <hendraabuhilmi@gmail.com>
2026-06-10 19:08:31 +0700

    Initial Commit

---
+++ Milo/MiloApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiloApp.swift
+//  Milo
+//
+//  Created by Hendra Irawan on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MiloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
